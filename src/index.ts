42ee24361e1752e749b96ebc9bfc2a7860fff6e1
Zeyad <zeyadw526@gmail.com>
2025-07-02 13:39:19 +0300

    connecting to mongo

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import userRouter from './routes/userRoutes';
 
 const app = express();
+
+const DB_USER = 'root';
+const DB_PASSWORD = 'example';
+const DB_PORT = '27017';
+const DB_HOST = 'mongo';
+
+const URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
+mongoose
+  .connect(URL)
+  .then(() => console.log('Database Connected Successfully'))
+  .catch(() => console.log('Database Connection Failed :('));
 
 app.use('/api/users', userRouter);
 
