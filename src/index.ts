416718ad8f37b30ba25ac58d7ffb4c6654b1de22
Zeyad <zeyadw526@gmail.com>
2025-07-02 15:34:46 +0300

    connecting redis with docker container

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
+import { createClient } from 'redis';
 import mongoose from 'mongoose';
 import userRouter from './routes/userRoutes';
 
 const app = express();
 
+const REDIS_PORT = 6379;
+const REDIS_HOST = 'redis';
+
+const redisClient = createClient({
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+
+redisClient.on('error', (err) => console.log('Redis Client Error', err));
+redisClient.on('connect', () => console.log('Redis Connected Successfully'));
+redisClient.connect();
+
 const DB_USER = 'root';
 const DB_PASSWORD = 'example';
-const DB_PORT = '27017';
+const DB_PORT = 27017;
 const DB_HOST = 'mongo';
 
 const URL = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
